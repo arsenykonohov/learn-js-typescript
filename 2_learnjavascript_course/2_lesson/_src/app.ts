e8bbc4af2b13095e6fe6fa8e3dd8d20d49d943af
arseny <arsenykonohov@gmail.com>
2016-08-19 13:17:46 +0300

    learnjavascript | 2_lesson - class work #4 - functions: special overloading signature

--- 2_learnjavascript_course/2_lesson/_src/app.ts
+++ 2_learnjavascript_course/2_lesson/_src/app.ts
@@ -38,29 +38,59 @@
 // console.log(getAverage(12, 12, 12, 12, 12));
 
 
+
+
+
 // FUNCTION OVERLOADING / FUNCTION OVERLOADING / FUNCTION OVERLOADING / FUNCTION OVERLOADING / FUNCTION OVERLOADING / FUNCTION OVERLOADING /
-function getAverage(a: number, b: number, c: number): string;
-function getAverage(a: string, b: string, c: string): string;
-// for example you can use this construct when you set certain HTMLelement-types (HTMLinput, etc...)
-function getAverage(a: any, b: any, c: any): string { // unspecial signature
-    // attension "parseInt" has strict type in "lib.d.ts" - typeScript src files
-    let total = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
-    let average = total / 3;
-    return `Result: ${average}`;
+
+// // unspecial (simple) overloading signature
+// // type definition:
+// function getAverage(a: number, b: number, c: number): string;    // unspecial signature
+// function getAverage(a: string, b: string, c: string): string;    // unspecial signature
+// // for example you can use this construct when you set certain HTMLelement-types (HTMLinput, etc...)
+// // signature with body:
+// function getAverage(a: any, b: any, c: any): string {            // unspecial signature
+//     // attension "parseInt" has strict type in "lib.d.ts" - typeScript src files
+//     let total = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
+//     let average = total / 3;
+//     return `Result: ${average}`;
+// };
+
+// // let someVar = getAverage(12, 23, 34);       // right way
+// // and...
+// let someVar = getAverage("12", "23", "34"); // this one too
+// console.log(someVar);
+
+// // ----------------------------------------------
+// // function getAverage(a: string|nymber, b: any, c: any): string { // unspecial (simple) overloading signature
+// //    // you can set "a" param as string or number - right way.
+// //    // code here....
+// //}
+// // let someVar =  getAverage(12, "23", "34");  // in case above
+// // ----------------------------------------------
+
+
+// special overloading signature:
+class Handler {}
+class RandomHandler {}
+class ReversedHandler {}
+
+// type definition:
+function getHandler(a: "Random"): RandomHandler;    // special signature
+function getHandler(a: "Reversed"): RandomHandler;  // special signature
+function getHandler(a: string): Handler;            // unspecial signature required in this case - looks like hack
+// signature with body:
+function getHandler(a: string): Handler {           // special signature
+    switch (a) {
+        case "Random":
+            return new RandomHandler();
+        case "Reversed":
+            return new ReversedHandler();
+        default:
+            return new Handler();
+    }
 };
-
-// let someVar = getAverage(12, 23, 34);       // right way
-// and...
-let someVar = getAverage("12", "23", "34"); // this one too
-console.log(someVar);
-
-// ----------------------------------------------
-// function getAverage(a: string|nymber, b: any, c: any): string { // unspecial signature 
-//    // you can set "a" param as string or number - right way.
-//    // code here....
-//}
-// let someVar =  getAverage(12, "23", "34");  // in case above
-// ----------------------------------------------
+getHandler("Reversed");
 
 
 
